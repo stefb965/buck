772f49ca3d86007949f87da7e380253b15f8ee7e
Simon Stewart <simons@fb.com>
2013-12-08 19:47:35 +0000

    Fix failing license check

--- src/org/openqa/selenium/buck/mozilla/Xpt.java
+++ src/org/openqa/selenium/buck/mozilla/Xpt.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.openqa.selenium.buck.mozilla;
 
 import com.facebook.buck.event.LogEvent;
@@ -5,6 +21,7 @@ import com.facebook.buck.rules.AbstractBuildable;
 import com.facebook.buck.rules.BuildContext;
 import com.facebook.buck.rules.BuildRuleParams;
 import com.facebook.buck.rules.BuildableContext;
+import com.facebook.buck.rules.RuleKey;
 import com.facebook.buck.rules.SourcePath;
 import com.facebook.buck.step.Step;
 import com.facebook.buck.step.fs.CopyStep;
@@ -12,6 +29,7 @@ import com.facebook.buck.step.fs.MakeCleanDirectoryStep;
 import com.facebook.buck.util.BuckConstant;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSortedSet;
 import com.google.common.io.Files;
 
@@ -37,6 +55,13 @@ public class Xpt extends AbstractBuildable {
   @Override
   public Iterable<String> getInputsToCompareToOutput() {
     return ImmutableSortedSet.of(src.toString());
+  }
+
+  @Override
+  public RuleKey.Builder appendDetailsToRuleKey(RuleKey.Builder builder) throws IOException {
+    return builder
+        .setInputs("src", ImmutableSet.of(src).iterator())
+        .setInputs("out", ImmutableSet.of(out).iterator());
   }
 
   @Override
