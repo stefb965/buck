81eb4e414c15ab70557f133f6da15c33fc5bc0d7
Uri Baghin <coneko@fb.com>
2015-11-18 05:46:46 -0800

    Move default rule key building logic to CachingBuildEngine.
    Summary: CachingBuildEngine currently handles all rule keys types except the default.
    This moves the rule key building logic to CachingBuildEngine together with
    the other types.
    
    Test Plan: CI
    
    Reviewed By: marcinkosiba
    
    fb-gh-sync-id: cbaaa87

--- src/com/facebook/buck/cli/TestCommand.java
+++ src/com/facebook/buck/cli/TestCommand.java
@@ -478,8 +478,7 @@ public class TestCommand extends BuildCommand {
     TargetGraphToActionGraph targetGraphToActionGraph =
         new TargetGraphToActionGraph(
             params.getBuckEventBus(),
-            new BuildTargetNodeToBuildRuleTransformer(),
-            params.getFileHashCache());
+            new BuildTargetNodeToBuildRuleTransformer());
     Pair<ActionGraph, BuildRuleResolver> actionGraphAndResolver =
         Preconditions.checkNotNull(targetGraphToActionGraph.apply(targetGraph));
 
