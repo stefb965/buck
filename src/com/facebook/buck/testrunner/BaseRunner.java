75e908714eece4c3dcb8ffb39e21e9ed924ecbaf
Simon Stewart <simon.m.stewart@gmail.com>
2016-10-07 20:46:26 +0100

    Output test results in a junit compatible format too

--- src/com/facebook/buck/testrunner/BaseRunner.java
+++ src/com/facebook/buck/testrunner/BaseRunner.java
@@ -33,6 +33,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Set;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -63,13 +64,68 @@ public abstract class BaseRunner {
 
   /**
    * The test result file is written as XML to avoid introducing a dependency on JSON (see class
-   * overview).
+   * overview). We use the format used by junit for fun and games.
    */
   protected void writeResult(String testClassName, List<TestResult> results)
       throws IOException, ParserConfigurationException, TransformerException {
     // XML writer logic taken from:
     // http://www.genedavis.com/library/xml/java_dom_xml_creation.jsp
 
+    Document buckDoc = createBuckOutput(testClassName, results);
+    prettyPrint(testClassName, ".xml", buckDoc);
+    Document junitDoc = createJUnitOutput(testClassName, results);
+    prettyPrint(testClassName, "-junit4.xml", junitDoc);
+  }
+
+  private void prettyPrint(String testClassName, String suffix, Document doc)
+      throws TransformerException, IOException {
+    // Create an XML transformer that pretty-prints with a 2-space indent.
+    // The transformer factory uses a system property to find the class to use. We need to default
+    // to the system default since we have the user's classpath and they may not have everything set
+    // up for the XSLT transform to work.
+    String vendor = System.getProperty("java.vm.vendor");
+    String factoryClass;
+    if ("IBM Corporation".equals(vendor)) {
+      // Used in the IBM JDK --- from
+      // https://www.ibm.com/support/knowledgecenter/SSYKE2_8.0.0/com.ibm.java.aix.80.doc/user/xml/using_xml.html
+      factoryClass = "com.ibm.xtq.xslt.jaxp.compiler.TransformerFactoryImpl";
+    } else {
+      // Used in the OpenJDK and the Oracle JDK.
+      factoryClass = "com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl";
+    }
+    // When we get this far, we're exiting, so no need to reset the property.
+    System.setProperty("javax.xml.transform.TransformerFactory", factoryClass);
+    TransformerFactory transformerFactory = TransformerFactory.newInstance();
+    Transformer trans = transformerFactory.newTransformer();
+    trans.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+    trans.setOutputProperty(OutputKeys.INDENT, "yes");
+    trans.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+
+    // Write the result to a file.
+    String testSelectorSuffix = "";
+    if (!testSelectorList.isEmpty()) {
+      testSelectorSuffix += ".test_selectors";
+    }
+    if (isDryRun) {
+      testSelectorSuffix += ".dry_run";
+    }
+    OutputStream output;
+    if (outputDirectory != null) {
+      File outputFile = new File(outputDirectory, testClassName + testSelectorSuffix + suffix);
+      output = new BufferedOutputStream(new FileOutputStream(outputFile));
+    } else {
+      output = System.out;
+    }
+    StreamResult streamResult = new StreamResult(output);
+    DOMSource source = new DOMSource(doc);
+    trans.transform(source, streamResult);
+    if (outputDirectory != null) {
+      output.close();
+    }
+  }
+
+  private Document createBuckOutput(String testClassName, List<TestResult> results)
+      throws ParserConfigurationException {
     DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
     Document doc = docBuilder.newDocument();
     doc.setXmlVersion("1.1");
@@ -122,49 +178,66 @@ public abstract class BaseRunner {
       root.appendChild(test);
     }
 
-    // Create an XML transformer that pretty-prints with a 2-space indent.
-    // The transformer factory uses a system property to find the class to use. We need to default
-    // to the system default since we have the user's classpath and they may not have everything set
-    // up for the XSLT transform to work.
-    String vendor = System.getProperty("java.vm.vendor");
-    String factoryClass;
-    if ("IBM Corporation".equals(vendor)) {
-      // Used in the IBM JDK --- from
-      // https://www.ibm.com/support/knowledgecenter/SSYKE2_8.0.0/com.ibm.java.aix.80.doc/user/xml/using_xml.html
-      factoryClass = "com.ibm.xtq.xslt.jaxp.compiler.TransformerFactoryImpl";
-    } else {
-      // Used in the OpenJDK and the Oracle JDK.
-      factoryClass = "com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl";
-    }
-    // When we get this far, we're exiting, so no need to reset the property.
-    System.setProperty("javax.xml.transform.TransformerFactory", factoryClass);
-    TransformerFactory transformerFactory = TransformerFactory.newInstance();
-    Transformer trans = transformerFactory.newTransformer();
-    trans.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
-    trans.setOutputProperty(OutputKeys.INDENT, "yes");
-    trans.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+    return doc;
+  }
 
-    // Write the result to a file.
-    String testSelectorSuffix = "";
-    if (!testSelectorList.isEmpty()) {
-      testSelectorSuffix += ".test_selectors";
+  private Document createJUnitOutput(String testClassName, List<TestResult> results)
+      throws ParserConfigurationException {
+    DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+    Document doc = docBuilder.newDocument();
+    doc.setXmlVersion("1.1");
+
+    Element testSuite = doc.createElement("testsuite");
+    testSuite.setAttribute("name", testClassName);
+    testSuite.setAttribute("tests", String.valueOf(results.size()));
+
+    long errorCount = 0;
+    float duration = 0;
+
+    for (TestResult result : results) {
+      Element testcase = doc.createElement("testcase");
+      testcase.setAttribute("name", result.testMethodName);
+      testcase.setAttribute("classname", result.testClassName);
+      testcase.setAttribute("time", String.format(Locale.US, "%.3f", (result.runTime / 1000f)));
+      duration += result.runTime;
+
+      if (!result.isSuccess()) {
+        errorCount++;
+        Element failure = doc.createElement("failure");
+        StringBuilder textContent = new StringBuilder();
+        if (result.failure != null) {
+          failure.setAttribute("message", result.failure.getMessage());
+          String stackTrace = stackTraceToString(result.failure);
+          textContent.append(stackTrace).append("\n");
+        } else {
+          failure.setAttribute("message", "An unknown failure occurred");
+        }
+
+        // stdout, if non-empty.
+        if (result.stdOut != null) {
+          textContent.append("====== STDOUT =====\n\n");
+          textContent.append(result.stdOut);
+          textContent.append("\n\n");
+        }
+
+        // stderr, if non-empty.
+        if (result.stdErr != null) {
+          textContent.append("====== STDERR =====\n\n");
+          textContent.append(result.stdErr);
+          textContent.append("\n\n");        }
+
+        failure.setTextContent(textContent.toString());
+        testcase.appendChild(failure);
+      }
+      testSuite.appendChild(testcase);
     }
-    if (isDryRun) {
-      testSelectorSuffix += ".dry_run";
-    }
-    OutputStream output;
-    if (outputDirectory != null) {
-      File outputFile = new File(outputDirectory, testClassName + testSelectorSuffix + ".xml");
-      output = new BufferedOutputStream(new FileOutputStream(outputFile));
-    } else {
-      output = System.out;
-    }
-    StreamResult streamResult = new StreamResult(output);
-    DOMSource source = new DOMSource(doc);
-    trans.transform(source, streamResult);
-    if (outputDirectory != null) {
-      output.close();
-    }
+
+    testSuite.setAttribute("failures", String.valueOf(errorCount));
+    testSuite.setAttribute("errors", "0");
+    testSuite.setAttribute("time", String.format("%.3f", (duration / 1000f)));
+    doc.appendChild(testSuite);
+    
+    return doc;
   }
 
   private String stackTraceToString(Throwable exc) {
